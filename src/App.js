6c00ab95d8b91e3c667420f849ab015ad105add2
TateFowble <tatefowble@gmail.com>
2021-04-14 01:39:28 -0500

    added start of navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
+import React, {
+  Component
+} from 'react';
+import {
+  HashRouter as Router,
+  // BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
 import './App.css';
+
+// Webpage imports
+import Navigation from './components/navigation/Navigation';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
-    <div>
-      
+    <div id="tatewbs">
+      <Navigation />
+      <Router>
+        <Switch>
+          <Homepage />
+        </Switch>
+      </Router>
     </div>
   );
 }
