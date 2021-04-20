4e6c7f2cfd87e8400f76c1a83a1cab7a4b87f7bf
TateFowble <tatefowble@gmail.com>
2021-04-20 17:06:25 -0500

    nearly done with footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,25 +1,35 @@
 import React from 'react';
 
+import './Footer.scss';
+
+import instagram from '../../assets/icons/instagram.svg';
+import github from '../../assets/icons/github.svg';
+import discord from '../../assets/icons/discord.svg';
+import twitch from '../../assets/icons/twitch.svg';
 
 function Footer() {
     return (
         <footer>
             <div className="
             d-flex
-             flex-column
+             flex-column-reverse
               align-items-center
                justify-content-center
                 flex-md-row
                  justify-content-md-around
                  ">
                 <div className="text-white">
-                    <p id="copyright-symbol">&#169;</p>
+                        <p id="copyright-symbol">&#169; TateWBS</p>
+                        <p id="copyright-undertext">A tech company</p>
                 </div>
                 <div className="text-white">
                     logo
                 </div>
-                <div className="text-white">
-                    Social
+                <div id="footer-social-media" className="text-white">
+                    <img src={ github  } alt="..." />
+                    <img src={ discord } alt="..." />
+                    <img src={ twitch  } alt="..." />
+                    <img src={ instagram } alt="..." />
                 </div>
             </div>
         </footer>
