536596ace1f81b396f7cc9f9837e12e0e62585bd
TateFowble <tatefowble@gmail.com>
2021-04-21 22:46:44 -0500

    added images

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -9,6 +9,9 @@ import Plx from 'react-plx';
 // images
 import reactjs from '../../assets/images/reactjs.png';
 import angularjs from '../../assets/images/angularjs.png';
+import nodejs from '../../assets/images/nodejs.png';
+import mysql from '../../assets/images/mysql.png';
+import mongodb from '../../assets/images/mongodb.png';
 
 // components
 import Box from '../../components/Box/box';
@@ -36,9 +39,16 @@ function Homepage() {
                     <h1 className="text">Oh hey there</h1>
                     <p className="text">(◕ᴥ◕ʋ)</p>
                 </div>
-                <div className="d-flex justify-content-evenly flex-row">
-                    <img src={reactjs} alt="" />
-                    <img src={angularjs} alt="" />
+                <div className="d-flex justify-content-around flex-column">
+                    <div>
+                        <img src={reactjs} alt="ReactJS" className="image" />
+                        <img src={angularjs} alt="AngularJS" className="image" />
+                        <img src={nodejs} alt="NodeJS" className="image" />
+                    </div>
+                    <div>
+                        <img src={mysql} alt="MySQL" className="image" />
+                        <img src={mongodb} alt="MongoDB" className="image" />
+                    </div>
                 </div>
             </div>
             <div id="homepage-section2" className="d-flex">
