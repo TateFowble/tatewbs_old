69ca74fa1c20bf2765dd40e2ea59078c31757795
TateFowble <tatefowble@gmail.com>
2021-06-29 23:16:22 -0500

    logo to url

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,40 +5,44 @@ import { Link } from 'react-router-dom';
 
 // import logo from '../../assets/images/logo21.png';
 
+// react-bootstrap imports
+import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Navigation() {
     return (
-            <nav className="navbar navbar-expand-md navbar-dark text-center">
-                <Link className="navbar-brand" to="/">TateWBS</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="collapsibleNavbar">
-                    <ul className="navbar-nav">
-                        <li class="nav-item">
-                            <Link className="nav-link" to="/about">About</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/services">Services</Link>
-                        </li>
-                        <li>
-                            <Link className="nav-link" to="/projects">Projects</Link>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="http://discord.tatewbs.com">Discord</a>
-                        </li>
-                        <li className="nav-item">
-                        <NavDropdown title="Links" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="http://youtube.tatewbs.com" target="_blank">YouTube</NavDropdown.Item>
-                            <NavDropdown.Item href="http://instagram.tatewbs.com" target="_blank">Instagram</NavDropdown.Item>
-                            <NavDropdown.Item href="http://twitch.tatewbs.com" target="_blank">Twitch</NavDropdown.Item>
-                            {/* <NavDropdown.Divider /> */}
-                        </NavDropdown>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+        <Navbar bg="dark" expand="lg">
+            </Navbar>
+            // <nav className="navbar navbar-expand-md navbar-dark text-center">
+            //     <Link className="navbar-brand" to="/">TateWBS</Link>
+            //     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            //         <span className="navbar-toggler-icon"></span>
+            //     </button>
+            //     <div className="collapse navbar-collapse" id="collapsibleNavbar">
+            //         <ul className="navbar-nav">
+            //             <li class="nav-item">
+            //                 <Link className="nav-link" to="/about">About</Link>
+            //             </li>
+            //             <li className="nav-item">
+            //                 <Link className="nav-link" to="/services">Services</Link>
+            //             </li>
+            //             <li>
+            //                 <Link className="nav-link" to="/projects">Projects</Link>
+            //             </li>
+            //             <li className="nav-item">
+            //                 <a className="nav-link" href="http://discord.tatewbs.com">Discord</a>
+            //             </li>
+            //             <li className="nav-item">
+            //             <NavDropdown title="Links" id="navbarScrollingDropdown">
+            //                 <NavDropdown.Item href="http://youtube.tatewbs.com" target="_blank">YouTube</NavDropdown.Item>
+            //                 <NavDropdown.Item href="http://instagram.tatewbs.com" target="_blank">Instagram</NavDropdown.Item>
+            //                 <NavDropdown.Item href="http://twitch.tatewbs.com" target="_blank">Twitch</NavDropdown.Item>
+            //                 {/* <NavDropdown.Divider /> */}
+            //             </NavDropdown>
+            //             </li>
+            //         </ul>
+            //     </div>
+            // </nav>
     )
 }
 
