76ad4cf5dbf83cd34d3d981376e94cd2bee8701a
TateFowble <tatefowble@gmail.com>
2022-09-03 04:11:02 -0500

    updated

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { db } from '../../firebase';
+// import { db } from '../../firebase';
 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -63,103 +63,104 @@ const ContactForm = (props) => {
 
         event.preventDefault();
 
-        db.collection("contact").doc(businessName)
-            .set({
-                name: name,
-                email: email,
-                phone: phone,
-                businessName: businessName,
-                notes: notes,
-                date: dateToString
-            })
-            .then(() => {
-                setEmail("");
-                setName("");
-                setPhone("");
-                setBusinessName("");
-                setNotes("");
-                return setFormWorked(true);
-            })
-            .catch((err) => {
-                alert(err.message);
-                return setFormWorked(false);
-            })
+        // db.collection("contact").doc(businessName)
+        //     .set({
+        //         name: name,
+        //         email: email,
+        //         phone: phone,
+        //         businessName: businessName,
+        //         notes: notes,
+        //         date: dateToString
+        //     })
+        //     .then(() => {
+        //         setEmail("");
+        //         setName("");
+        //         setPhone("");
+        //         setBusinessName("");
+        //         setNotes("");
+        //         return setFormWorked(true);
+        //     })
+        //     .catch((err) => {
+        //         alert(err.message);
+        //         return setFormWorked(false);
+        //     })
 
     };
 
 
     return (
-        <div className="mt-5 mb-5">
-            <p className="fs-2 font-weight-bold m-0">{page}</p>
-            <p className="font-italic mb-5">
-                Email or fill in the form below!
-            </p>
-            <p className="fs-3"><u>info@tatewbs.com</u></p>
-            <hr className="mb-5 mt-4" />
-            <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-4">
-                    <Row>
-                        <Col>
-                            <Form.Control
-                                type="text"
-                                placeholder="Name*"
-                                value={name}
-                                onChange={((event) => setName(event.target.value))} 
-                                required
-                                />
-                        </Col>
-                        <Col>
-                            <Form.Control
-                                type="text"
-                                placeholder="Email*"
-                                value={email}
-                                onChange={((event) => setEmail(event.target.value))} 
-                                required
-                                />
-                        </Col>
-                    </Row>
-                </Form.Group>
-                <Form.Group className="mb-4">
-                    <Row>
-                        <Col>
-                            <Form.Control
-                                type="text"
-                                placeholder="Business Name*"
-                                value={businessName}
-                                onChange={((event) => setBusinessName(event.target.value))} 
-                                required
-                                />
-                        </Col>
-                        <Col>
-                            <Form.Control
-                                type="text"
-                                placeholder="Phone"
-                                value={phone}
-                                onChange={((event) => setPhone(event.target.value))}
-                                required
-                            />
-                        </Col>
-                    </Row>
-                </Form.Group>
-                <Form.Group className="mb-4">
-                    <Row>
-                        <Col>
-                            <Form.Control
-                                as="textarea"
-                                rows={4}
-                                placeholder="Notes"
-                                value={notes}
-                                onChange={((event) => setNotes(event.target.value))} />
-                        </Col>
-                    </Row>
-                </Form.Group>
-                <Button type="submit">Submit</Button>
-            </Form>
-            <div className="mt-3">
-                {formNotifier(formWorked)}
-            </div>
-        </div>
-    );
+        // <div className="mt-5 mb-5">
+        //     <p className="fs-2 font-weight-bold m-0">{page}</p>
+        //     <p className="font-italic mb-5">
+        //         Email or fill in the form below!
+        //     </p>
+        //     <p className="fs-3"><u>info@tatewbs.com</u></p>
+        //     <hr className="mb-5 mt-4" />
+        //     <Form onSubmit={handleSubmit}>
+        //         <Form.Group className="mb-4">
+        //             <Row>
+        //                 <Col>
+        //                     <Form.Control
+        //                         type="text"
+        //                         placeholder="Name*"
+        //                         value={name}
+        //                         onChange={((event) => setName(event.target.value))} 
+        //                         required
+        //                         />
+        //                 </Col>
+        //                 <Col>
+        //                     <Form.Control
+        //                         type="text"
+        //                         placeholder="Email*"
+        //                         value={email}
+        //                         onChange={((event) => setEmail(event.target.value))} 
+        //                         required
+        //                         />
+        //                 </Col>
+        //             </Row>
+        //         </Form.Group>
+        //         <Form.Group className="mb-4">
+        //             <Row>
+        //                 <Col>
+        //                     <Form.Control
+        //                         type="text"
+        //                         placeholder="Business Name*"
+        //                         value={businessName}
+        //                         onChange={((event) => setBusinessName(event.target.value))} 
+        //                         required
+        //                         />
+        //                 </Col>
+        //                 <Col>
+        //                     <Form.Control
+        //                         type="text"
+        //                         placeholder="Phone"
+        //                         value={phone}
+        //                         onChange={((event) => setPhone(event.target.value))}
+        //                         required
+        //                     />
+        //                 </Col>
+        //             </Row>
+        //         </Form.Group>
+        //         <Form.Group className="mb-4">
+        //             <Row>
+        //                 <Col>
+        //                     <Form.Control
+        //                         as="textarea"
+        //                         rows={4}
+        //                         placeholder="Notes"
+        //                         value={notes}
+        //                         onChange={((event) => setNotes(event.target.value))} />
+        //                 </Col>
+        //             </Row>
+        //         </Form.Group>
+        //         <Button type="submit">Submit</Button>
+        //     </Form>
+        //     <div className="mt-3">
+        //         {formNotifier(formWorked)}
+        //     </div>
+        // </div>
+    <></>
+        );
 }
 // Hello, my name is _____ and I am needing to talk about a(n) ______ for my business _______.
 
