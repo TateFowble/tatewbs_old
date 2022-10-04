328f060363242f30bee2b9bc2f562f9eaeb3b1e4
TateFowble <tatefowble@gmail.com>
2022-10-03 22:57:21 -0500

    making the project cards looking better

--- src/pages/projectpage.jsx
+++ src/pages/projectpage.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
-
-// images
-import tatewbsImage from '../assets/images/tatewbs.png';
 
 const ProjectPage = () => {
 
@@ -13,62 +10,57 @@ const ProjectPage = () => {
         {
             siteName: 'TateWBS',
             siteURL: 'https://www.tatewbs.com',
-            siteDescription: 'This is the site you\'re currently on',
-            siteImage: tatewbsImage
+            siteDescription: 'This is the site you\'re currently on'
         },
         {
             siteName: 'StarWorksMC',
             siteURL: 'https://www.starworksmc.us',
             siteDescription: 'A Minecraft Network Website that includes multiple servers and api\'s integrated into it',
-            siteImage: ''
         },
         {
             siteName: 'ActuallyArcher',
             siteURL: 'https://www.actuallyarcher.com',
             siteDescription: 'An eCommerce site',
-            siteImage: ''
         },
         {
             siteName: 'El Maya',
             siteURL: 'https://menu.elmayamx.com',
             siteDescription: 'A mexican restaurant menu',
-            siteImage: ''
         },
         {
             siteName: 'FoamTech',
             siteURL: 'https://foamtech.tatewbs.com',
-            siteDescription: 'FoamTechInc Web app',
-            siteImage: ''
+            siteDescription: 'FoamTechInc Web app'
         }
     ]
 
-    const siteArr = (name, url, description, image) => {
-        return (
-            <div className='m-5 w-100 shadow'>
-                <a href={url} target='_blank' rel='noreferrer'>
-                    <Card className='bg-dark text-white'>
-                        <Card.Img src={image || ``} alt='' height='200px' />
-                        <Card.ImgOverlay className='text-center'>
-                            <Card.Title className='mb-5'>{name}</Card.Title>
-                            <Card.Text>{description}</Card.Text>
-                        </Card.ImgOverlay>
-                    </Card>
-                </a>
-            </div>
-        );
-    }
-
-    document.title = `TateWBS | Projects`;
+    useEffect(() => {
+        document.title = `TateWBS | Projects`;
+    }, []);
     return (
         <Container fluid={true}>
-            <div className='d-flex flex-wrap flex-row'>
+            <div className='d-flex flex-wrap bf-dark'>
                 {
                     sites.map((obj, i) => {
-                        let siteName = obj.siteName;
-                        let siteURL = obj.siteURL;
-                        let siteDescription = obj.siteDescription;
-                        let siteImage = obj.siteImage;
-                        return siteArr(siteName, siteURL, siteDescription, siteImage);
+                        let name = obj.siteName;
+                        let url = obj.siteURL;
+                        let description = obj.siteDescription;
+                        let image = obj.siteImage;
+                        return (
+                            <Card className='text-white' style={{ background: '' }}>
+                                <a
+                                    key={i}
+                                    href={url}
+                                    target='_blank'
+                                    rel='noreferrer'>
+                                    <Card.Img src={image || ``} alt='' height='200px' />
+                                    <Card.ImgOverlay className='text-center'>
+                                        <Card.Title className='mb-5'>{name}</Card.Title>
+                                        <Card.Text>{description}</Card.Text>
+                                    </Card.ImgOverlay>
+                                </a>
+                            </Card>
+                        )
                     })
                 }
             </div>
@@ -77,3 +69,15 @@ const ProjectPage = () => {
 }
 
 export default ProjectPage;
+
+
+
+/*
+
+Black
+Purple
+Blue
+Red
+Gray
+
+*/
